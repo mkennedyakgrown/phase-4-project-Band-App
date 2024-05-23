1bf8b3a1cdfd2808e657a84064da3ef67d8812fe
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-05-22 23:18:00 -0500

    add redirect if not logged in to ConfigPage

--- client/src/pages/ConfigPage.jsx
+++ client/src/pages/ConfigPage.jsx
@@ -1,10 +1,14 @@
 import { Divider, Header } from "semantic-ui-react";
 import CreateGenreForm from "../components/CreateGenreForm";
 import CreateInstrumentForm from "../components/CreateInstrumentForm";
+import { Navigate, useOutletContext } from "react-router-dom";
 
 function ConfigPage() {
+  const { user } = useOutletContext();
   return (
     <>
+      {/* Redirect to login if user is not logged in */}
+      {user.id ? null : <Navigate to="/login" />}
       <Header as="h1">Config Page</Header>
       <Divider />
       <CreateGenreForm />
