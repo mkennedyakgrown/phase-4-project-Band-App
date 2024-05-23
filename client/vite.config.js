6612523fef00635ab6368c5beb82031f4c29afc1
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-05-23 12:45:09 -0500

    add psycopg2 to requirements, update port on vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         // target: "http://127.0.0.1:5555",
+        host: true,
+        strictPort: true,
         port: 8000,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
