cd57e75b2160a805c7e503317191caec4e769fbf
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-05-22 23:54:14 -0500

    change Vite port

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5555",
+        // target: "http://127.0.0.1:5555",
+        port: 8000,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
